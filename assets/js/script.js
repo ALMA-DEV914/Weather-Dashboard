d8e5845e5e0c2b024a4b098b6de889292be13454
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-10 21:10:20 -0800

    Update color text for UV link

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ function currentForecast(city) {
                             uvindexEl.setAttribute("style", "background:green");
                         }
                         else if(data.current.uvi >= 5 && data.current.uvi < 10){
-                            uvindexEl.setAttribute("style", "background: yellow");
+                            uvindexEl.setAttribute("style", "color: black");
                         }
                         else{
                             uvindexEl.setAttribute("style", "background:red");
